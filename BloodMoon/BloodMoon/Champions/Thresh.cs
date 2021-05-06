ff3f5327cb102fb70eaecc9dbeadcecf315a2a3b
Decaed <troy.ando@hotmail.com>
2021-05-06 11:55:44 +1000

    Update Thresh.cs

--- BloodMoon/BloodMoon/Champions/Thresh.cs
+++ BloodMoon/BloodMoon/Champions/Thresh.cs
@@ -6,6 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using FunnySlayerCommon;
+using SPredictionMash;
+
+SPredictionMash.ConfigMenu.Initialize(HelperMenu, "Get Prediction");
 
 namespace BloodMoon.Champions
 {
