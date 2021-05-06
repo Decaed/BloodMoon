58cbccacfc7d6c8e894000c0cb51bef6e9415a89
Decaed <troy.ando@hotmail.com>
2021-05-06 16:06:54 +1000

    Update Thresh.cs

--- BloodMoon/BloodMoon/Champions/Thresh.cs
+++ BloodMoon/BloodMoon/Champions/Thresh.cs
@@ -72,6 +72,7 @@ namespace BloodMoon.Champions
             Config.Attach();
 
             GameEvent.OnGameTick += OnGameUpdate;
+            dashingQ();
             Drawing.OnDraw += OnDraw;
         }
 
@@ -141,7 +142,7 @@ namespace BloodMoon.Champions
         {
             if (Q.IsReady())
             {
-                var target = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
+                var target = TargetSelector.GetTarget(Q.Range);
                 var Player = GameObjects.Player;
                 var input = Q.GetPrediction(target, true);
 
@@ -152,6 +153,28 @@ namespace BloodMoon.Champions
                 return;
                         
                 if (input.Hitchance >= HitChance.High)
+                {
+                    Q.Cast(input.CastPosition);
+                }
+
+            }
+        }
+        
+                private static void dashingQ()
+        {
+            if (Q.IsReady())
+            {
+                var target = TargetSelector.GetTarget(Q.Range);
+                var Player = GameObjects.Player;
+                var input = Q.GetPrediction(target, true);
+
+                if (!target.IsValidTarget())
+                    return;
+
+                if (target.HasBuff("threshQ"))
+                return;
+                        
+                if (input.Hitchance >= HitChance.Dashing)
                 {
                     Q.Cast(input.CastPosition);
                 }
