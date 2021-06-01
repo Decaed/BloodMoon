212bbf405ebc800b4dd51d3c82dea6872395d97a
Decaed <troy.ando@hotmail.com>
2021-06-01 19:30:23 +1000

    Update Thresh.cs

--- BloodMoon/BloodMoon/Champions/Thresh.cs
+++ BloodMoon/BloodMoon/Champions/Thresh.cs
@@ -141,7 +141,7 @@ namespace BloodMoon.Champions
         {
             if (Q.IsReady())
             {
-                var target = Q.GetTarget();
+                var target = FunnySlayerCommon.FSTargetSelector.GetFSTarget(Q.Range);
                 var Player = GameObjects.Player;
                 var input = Q.GetPrediction(target, true);
 
