013807502ad5da089b56d77cb4a1c41b763e2c4e
Decaed <troy.ando@hotmail.com>
2021-06-13 11:06:41 +1000

    Update Thresh.cs

--- BloodMoon/BloodMoon/Champions/Thresh.cs
+++ BloodMoon/BloodMoon/Champions/Thresh.cs
@@ -50,7 +50,6 @@ namespace BloodMoon.Champions
 
             GameEvent.OnGameTick += OnGameUpdate;
             Drawing.OnDraw += OnDraw;
-            Obj_AI_Hero.OnProcessSpellCast += Game_ProcessSpell;
         } 
 
         public static void OnGameUpdate(EventArgs args)
