1c19ce6c63bfb00010a5948b2f9f09cc8bf8be14
Decaed <troy.ando@hotmail.com>
2021-09-17 14:29:25 +1000

    Update Thresh.cs

--- BloodMoon/BloodMoon/Champions/Thresh.cs
+++ BloodMoon/BloodMoon/Champions/Thresh.cs
@@ -143,7 +143,7 @@ namespace BloodMoon.Champions
         
         private static void logicE()
   {
-      var target = targets.Closest; ;
+      var target = target.Closest; ;
       var Player = GameObjects.Player;
 
       if (E.IsReady() && target.DistanceToPlayer() < E.Range)
