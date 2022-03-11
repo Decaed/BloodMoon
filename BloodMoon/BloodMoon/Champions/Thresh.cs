f287325042d8bc7faedc8d95d7f5e0830f241c68
Decaed <troy.ando@hotmail.com>
2022-03-12 09:55:03 +1100

    Update Thresh.cs

--- BloodMoon/BloodMoon/Champions/Thresh.cs
+++ BloodMoon/BloodMoon/Champions/Thresh.cs
@@ -13,7 +13,7 @@ namespace BloodMoon.Champions
     public class Thresh
     {
         private static AIHeroClient ME = GameObjects.Player;
-        private static Spell Q, W, E;
+        private static Spell Q, W, E, R;
         private static Menu Config;
 
         public static void OnGameLoad()
