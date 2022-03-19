0d996f9e71bfa4f6f2d3819412630842f1d4f66a
Decaed <troy.ando@hotmail.com>
2022-03-20 03:56:11 +1100

    Update Thresh.cs

--- BloodMoon/BloodMoon/Champions/Thresh.cs
+++ BloodMoon/BloodMoon/Champions/Thresh.cs
@@ -62,6 +62,7 @@ namespace BloodMoon.Champions
             {
                 case OrbwalkerMode.Combo:
                     logicE();
+                    break;
                     logicQ();
                     break;
 
