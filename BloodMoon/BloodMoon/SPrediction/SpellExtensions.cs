2387dcb686e2bcf976dd54acdd612a4626f1389b
Decaed <troy.ando@hotmail.com>
2022-03-20 04:38:54 +1100

    Update SpellExtensions.cs

--- BloodMoon/BloodMoon/SPrediction/SpellExtensions.cs
+++ BloodMoon/BloodMoon/SPrediction/SpellExtensions.cs
@@ -278,48 +278,6 @@ namespace SPrediction
             /*if (!s.IsSkillShot)
                 return Vector3.Zero;*/
 
-            #region if common prediction selected
-            if (ConfigMenu.SelectedPrediction.Index == 1)
-            {
-                var pout = s.GetPrediction(t, minHit > 1);
-
-                if (minHit > 1)
-                    if (pout.AoeTargetsHitCount >= minHit)
-                        return pout.CastPosition;
-
-                if (pout.Hitchance >= hc)
-                    return pout.CastPosition;
-            }
-            #endregion
-
-            #region if fs prediction selected
-            if (ConfigMenu.SelectedPrediction.Index == 2)
-            {
-                var pout = Prediction.GetPrediction(s, t, minHit > 1);
-
-                if (minHit > 1)
-                    if (pout.AoeTargetsHitCount >= minHit)
-                        return pout.CastPosition;
-
-                if ((int)pout.Hitchance >= (int)hc)
-                    return pout.CastPosition;
-            }
-            #endregion
-
-            #region If Exory Prediction Selected
-            if (ConfigMenu.SelectedPrediction.Index == 3)
-            {
-                var pout = SebbyLibPorted.Prediction.Prediction.GetPrediction(s, t, minHit > 1);
-
-                if (minHit > 1)
-                    if (pout.AoeTargetsHitCount >= minHit)
-                        return pout.CastPosition;
-
-                if ((int)pout.Hitchance >= (int)hc)
-                    return pout.CastPosition;
-            }
-            #endregion
-
             /*if (minHit > 1)
                 return SPredictionCastAoe(s, minHit);*/
 
