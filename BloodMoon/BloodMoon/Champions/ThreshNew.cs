d4ebf569aa59e684bdd2656946accb351cee24ea
Decaed <troy.ando@hotmail.com>
2023-03-26 10:38:21 +1100

    Update ThreshNew.cs

--- BloodMoon/BloodMoon/Champions/ThreshNew.cs
+++ BloodMoon/BloodMoon/Champions/ThreshNew.cs
@@ -39,3 +39,49 @@ namespace BloodMoon.Champions
             GameEvent.OnGameTick += OnGameUpdate;
             AntiGapcloser.OnGapcloser += Gapcloser_OnGapcloser;
         }
+        public static void OnGameUpdate(EventArgs args)
+        {
+            AutoCast();
+
+            switch (Orbwalker.ActiveMode)
+            {
+                case OrbwalkerMode.Combo:
+                    logicE();
+                    CastQ();
+                    break;
+                // Add cases for other modes if necessary
+            }
+        }
+
+        private static void Gapcloser_OnGapcloser(AIHeroClient sender, AntiGapcloser.GapcloserArgs args)
+        {
+            if (sender.IsAlly || args.SpellName == "ZedR")
+            {
+                return;
+            }
+
+            if (args.EndPosition.DistanceToPlayer() <= 500 && sender.IsValidTarget(1000))
+            {
+                if (E.Cast(sender) == CastStates.SuccessfullyCasted)
+                {
+                    return;
+                }
+            }
+        }
+
+        private static void CastQ()
+        {
+            var target = Q.GetTarget(Q.Range);
+
+            if (target == null || target.HasBuff("threshq") || ObjectManager.Player.Distance(target.Position) < 500)
+            {
+                return;
+            }
+
+            var prediction = Q.GetPrediction(target);
+
+            if (prediction.Hitchance >= (HitChance.High))
+            {
+                Q.Cast(prediction.CastPosition);
+            }
+        }
